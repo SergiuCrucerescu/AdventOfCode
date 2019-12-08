395503809ff89e531a1b890dd32a0e24ea2373fb
Sergiu Crucerescu <sergiu.crucerescu@gmail.com>
2019-12-08 23:15:07 +0200

    Day 1 Added tests

--- src/day1/Day1.kt
+++ src/day1/Day1.kt
@@ -1,16 +1,26 @@
 package day1
 
 import java.io.File
+import kotlin.test.assertEquals
 
 fun main() {
     val result = File("day1.txt").useLines { processInput(it) }
+
+    assertEquals(2, computeFuelSequence(12).sum())
+    assertEquals(2, computeFuelSequence(14).sum())
+    assertEquals(966, computeFuelSequence(1969).sum())
+    assertEquals(50346, computeFuelSequence(100756).sum())
+
     println(result)
 }
 
 fun processInput(lines: Sequence<String>): Int =
     lines.map(String::toInt)
-        .flatMap { mass ->
-            generateSequence(mass, { it / 3 - 2 })
-                .drop(1)
-                .takeWhile { it > 0 }
-        }.sum()
+        .flatMap { computeFuelSequence(it) }
+        .sum()
+
+fun computeFuelSequence(mass: Int): Sequence<Int> {
+    return generateSequence(mass, { it / 3 - 2 })
+        .drop(1)
+        .takeWhile { it > 0 }
+}
